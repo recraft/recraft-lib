fc4e519006f5819a2c58cea72e7d0dfe69c36caa
imdadadani <dadadani2134@gmail.com>
2020-05-30 19:13:39 +0200

    refactoring

--- utils/binaryStruct.go
+++ utils/binaryStruct.go
@@ -11,7 +11,7 @@ import (
 	"github.com/recraft/recraft-lib/types"
 )
 
-//Get the original field name by tag
+// Get the original field name by tag
 func getField(count string, rt reflect.Type) (fieldName string) {
 	if rt.Kind() != reflect.Struct {
 		panic("bad type")
@@ -25,22 +25,22 @@ func getField(count string, rt reflect.Type) (fieldName string) {
 	return ""
 }
 
-//BinaryToStruct converts main data types from the Minecraft protcols directly to any struct.
-//The order can be know by tags.
-//Note: This function depends on reflect, any error can give a panic.
+// BinaryToStruct converts main data types from the Minecraft protcols directly to any struct.
+// The order can be know by tags.
+// Note: This function depends on reflect, any error can give a panic, it should be modified.
 func BinaryToStruct(structt interface{}, reader *bufio.Reader) error {
-	//Get the original struct
+	// Get the original struct
 	valueOf := reflect.ValueOf(structt).Elem()
 	typeOf := reflect.TypeOf(structt).Elem()
 
-	//Check if type is struct
+	// Check if type is struct
 	if valueOf.Kind() != reflect.Struct {
 		return errors.New("bad type")
 	}
-	//Get fields by couting using tag(s)
+	// Get fields by couting using tag(s)
 	for rcount := 1; rcount <= valueOf.NumField(); rcount++ {
 
-		//Get the name of the field using a tag
+		// Get the name of the field using a tag
 		field := getField(strconv.Itoa(rcount), typeOf)
 		fieldSettings := valueOf.FieldByName(field)
 
@@ -119,92 +119,66 @@ func BinaryToStruct(structt interface{}, reader *bufio.Reader) error {
 	return nil
 }
 
-//StructToBinary acts like BinaryToStruct but doing the opposite thing, converting a struct into binary
+// StructToBinary converts any structs to binary data types from the Minecraft protocol.
+// The order can be know by tags.
+// Note: This function depends on reflect, any error can give a panic, it should be modified.
 func StructToBinary(structt interface{}, packetID int32) ([]byte, error) {
 
-	//Get the original struct
-	valueOf := reflect.ValueOf(structt).Elem()
-	typeOf := reflect.TypeOf(structt).Elem()
-
-	//Check if type is struct
-	if valueOf.Kind() != reflect.Struct {
-		return []byte{}, errors.New("bad type")
-	}
 	buf := make([]byte, 0)
 	buf = append(buf, types.VarInt(packetID).GetBytes()...)
 
-	//Get fields by couting using tag(s)
-	for rcount := 1; rcount <= valueOf.NumField(); rcount++ {
-		field := getField(strconv.Itoa(rcount), typeOf)
-		fieldSettings := valueOf.FieldByName(field)
+	if structt != nil {
+		// Get the original struct
+		valueOf := reflect.ValueOf(structt).Elem()
+		typeOf := reflect.TypeOf(structt).Elem()
 
-		//var Type types.BasicTypes
-
-		switch fmt.Sprint(fieldSettings.Type()) {
-		case "types.String":
-			s := fieldSettings.Interface().(types.String)
-			buf = append(buf, s.GetBytes()...)
-
-			break
-		case "types.Boolean":
-			b := fieldSettings.Interface().(types.Boolean)
-			buf = append(buf, b.GetBytes()...)
-
-			break
-		case "types.Short":
-			s := fieldSettings.Interface().(types.Short)
-			buf = append(buf, s.GetBytes()...)
-
-			break
-		case "types.UnsignedShort":
-			us := fieldSettings.Interface().(types.UnsignedShort)
-			buf = append(buf, us.GetBytes()...)
-
-			break
-		case "types.Int":
-			i := fieldSettings.Interface().(types.Int)
-			buf = append(buf, i.GetBytes()...)
-
-			break
-		case "types.Long":
-			l := fieldSettings.Interface().(types.Long)
-			buf = append(buf, l.GetBytes()...)
-
-			break
-		case "types.VarInt":
-			vi := fieldSettings.Interface().(types.VarInt)
-			buf = append(buf, vi.GetBytes()...)
-
-			break
-		case "types.VarLong":
-			vl := fieldSettings.Interface().(types.VarLong)
-			buf = append(buf, vl.GetBytes()...)
-
-			break
-
-			/*case "types.Boolean":
-				Type = types.Boolean(fieldSettings.Bool())
-				break
-			case "types.Int":
-				Type = types.Int(fieldSettings.Int())
-				break
-			case "types.Long":
-				Type = types.Long(fieldSettings.Int())
-				break
-			case "types.Short":
-				Type = types.Short(fieldSettings.Int())
-				break
-			case "types.VarInt":
-				Type = types.VarInt(fieldSettings.Int())
-				break
-			case "types.VarLong":
-				Type = types.VarLong(fieldSettings.Int())
-				break*/
-		default:
-			return []byte{}, errors.New("Type of rcount" + strconv.Itoa(rcount) + " not supported")
-
+		// Check if type is struct
+		if valueOf.Kind() != reflect.Struct {
+			return []byte{}, errors.New("bad type")
 		}
 
+		// Get fields by couting using tag(s)
+		for rcount := 1; rcount <= valueOf.NumField(); rcount++ {
+			field := getField(strconv.Itoa(rcount), typeOf)
+			fieldSettings := valueOf.FieldByName(field)
+
+			switch fmt.Sprint(fieldSettings.Type()) {
+			case "types.String":
+				s := fieldSettings.Interface().(types.String)
+				buf = append(buf, s.GetBytes()...)
+				break
+			case "types.Boolean":
+				b := fieldSettings.Interface().(types.Boolean)
+				buf = append(buf, b.GetBytes()...)
+				break
+			case "types.Short":
+				s := fieldSettings.Interface().(types.Short)
+				buf = append(buf, s.GetBytes()...)
+				break
+			case "types.UnsignedShort":
+				us := fieldSettings.Interface().(types.UnsignedShort)
+				buf = append(buf, us.GetBytes()...)
+				break
+			case "types.Int":
+				i := fieldSettings.Interface().(types.Int)
+				buf = append(buf, i.GetBytes()...)
+				break
+			case "types.Long":
+				l := fieldSettings.Interface().(types.Long)
+				buf = append(buf, l.GetBytes()...)
+				break
+			case "types.VarInt":
+				vi := fieldSettings.Interface().(types.VarInt)
+				buf = append(buf, vi.GetBytes()...)
+				break
+			case "types.VarLong":
+				vl := fieldSettings.Interface().(types.VarLong)
+				buf = append(buf, vl.GetBytes()...)
+				break
+			default:
+				return []byte{}, errors.New("Type of rcount" + strconv.Itoa(rcount) + " not supported")
+			}
+		}
 	}
 
 	binary := make([]byte, 0)
